b5bbea94ccb3e8cca57347b181d76e4330ce5def
BackJoaco <joaquingaricano@gmail.com>
2024-06-20 14:57:39 -0300

    Cambios de estilo en el NavBar y correcciones visuales

--- inmobiliaria/src/components/NavBarComponent.jsx
+++ inmobiliaria/src/components/NavBarComponent.jsx
@@ -3,10 +3,10 @@ const NavBarComponent =()=>{
     return (
       <nav className="navbar">
         <ul>
-        <li><a href="/tipos_Propiedad">TipoPropiedades</a></li>
-            <li><a href="/propiedad">Propiedades</a></li>
-            <li><a href="/reservas">Reservas</a></li>
-            
+          <li><a href="/tipos_Propiedad">Tipos de propiedades</a></li>
+          <li><a href="/propiedad">Propiedades</a></li>
+          <li><a href="/reservas">Reservas</a></li>
+          <li><a href="/tipos_Propiedad">TipoPropiedades</a></li>
         </ul>
       </nav>
     )
